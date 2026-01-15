3b3386076e18eadc1ae7187621f3e565dda91c8f
Mustafa ÖNCEL <mustafaoncel92@gmail.com>
2026-01-15 23:28:30 +0300

    Disable all glowing

--- src/main/java/gg/darkutils/mixin/performance/EntityMixin.java
+++ src/main/java/gg/darkutils/mixin/performance/EntityMixin.java
@@ -2,37 +2,23 @@ package gg.darkutils.mixin.performance;
 
 import gg.darkutils.config.DarkUtilsConfig;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.ItemEntity;
 import org.jetbrains.annotations.NotNull;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Entity.class)
 public final class EntityMixin {
-    @Unique
-    @Final
-    private boolean darkutils$item;
-
     private EntityMixin() {
         super();
 
         throw new UnsupportedOperationException("mixin class");
     }
 
-    @Inject(method = "<init>", at = @At("RETURN"))
-    private void darkutils$init(@NotNull final CallbackInfo ci) {
-        final var entity = (Entity) (Object) this;
-        this.darkutils$item = entity instanceof ItemEntity;
-    }
-
     @Inject(method = "isGlowing", at = @At("HEAD"), cancellable = true)
     public final void darkutils$isGlowing$disableIfEnabled(@NotNull final CallbackInfoReturnable<Boolean> cir) {
-        if (DarkUtilsConfig.INSTANCE.disableGlowing && this.darkutils$item) {
+        if (DarkUtilsConfig.INSTANCE.disableGlowing) {
             cir.setReturnValue(false);
         }
     }
