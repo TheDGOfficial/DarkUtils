0f3489d2441a6d16dc2bdeee7ebbdedc311b00f7
Mustafa ÖNCEL <mustafaoncel92@gmail.com>
2026-02-09 04:20:51 +0300

    Only reassign lastInputTime once on initialization

--- src/main/java/gg/darkutils/mixin/bugfixes/InactivityFpsLimiterMixin.java
+++ src/main/java/gg/darkutils/mixin/bugfixes/InactivityFpsLimiterMixin.java
@@ -6,14 +6,18 @@ import net.minecraft.util.Util;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(InactivityFpsLimiter.class)
 final class InactivityFpsLimiterMixin {
     @Shadow
     private long lastInputTime;
+    @Unique
+    private boolean uninitialized;
 
     private InactivityFpsLimiterMixin() {
         super();
@@ -21,9 +25,15 @@ final class InactivityFpsLimiterMixin {
         throw new UnsupportedOperationException("mixin class");
     }
 
+    @Inject(method = "<init>", at = @At("HEAD"))
+    private final void darkutils$init(@NotNull final CallbackInfo ci) {
+        this.uninitialized = true;
+    }
+
     @Inject(method = "getLimitReason", at = @At("HEAD"), cancellable = true)
     private final void darkutils$fixInactivityFpsLimiterIfEnabled(@NotNull final CallbackInfoReturnable<InactivityFpsLimiter.LimitReason> cir) {
-        if (DarkUtilsConfig.INSTANCE.fixInactivityFpsLimiter && 0L == this.lastInputTime) {
+        if (DarkUtilsConfig.INSTANCE.fixInactivityFpsLimiter && this.uninitialized && 0L == this.lastInputTime) {
+            this.uninitialized = false;
             this.lastInputTime = Util.getMeasuringTimeMs();
         }
     }
