1f2fce0853ac47e40c11730dbb368d0c0905cc5d
Mustafa ÖNCEL <mustafaoncel92@gmail.com>
2026-02-26 02:41:03 +0300

    Backup config if loading fails

--- src/main/java/gg/darkutils/config/DarkUtilsConfig.java
+++ src/main/java/gg/darkutils/config/DarkUtilsConfig.java
@@ -2,19 +2,25 @@ package gg.darkutils.config;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+
 import gg.darkutils.DarkUtils;
 import gg.darkutils.events.ConfigSaveFinishEvent;
 import gg.darkutils.events.ConfigSaveStartEvent;
 import gg.darkutils.feat.performance.OpenGLVersionOverride;
 import gg.darkutils.utils.LogLevel;
+
 import net.fabricmc.loader.api.FabricLoader;
+
 import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
+
 import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
 import java.io.IOException;
+
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
 import java.nio.file.Files;
 
 public final class DarkUtilsConfig {
@@ -123,16 +129,24 @@ public final class DarkUtilsConfig {
                 return DarkUtilsConfig.GSON.fromJson(Files.readString(DarkUtilsConfig.FILE.toPath(), StandardCharsets.UTF_8), DarkUtilsConfig.class);
             } catch (final IOException e) {
                 DarkUtils.error(DarkUtilsConfig.class, "Unable to load config", e);
+                DarkUtils.warn(DarkUtilsConfig.class, "Backing up corrupted config due to load failure as the next save will revert to the default config. If you wish to restore your settings manually, check the file ending with .corrupted extension.");
+
+                // This saves to a seperate file with the same name but .corrupted at the end, e.g., config.json.corrupted.
+                DarkUtilsConfig.save(DarkUtilsConfig.FILE.toPath().resolveSibling(DarkUtilsConfig.FILE.toPath().getFileName() + ".corrupted"));
             }
         }
-        return new DarkUtilsConfig();
+        return new DarkUtilsConfig(); // Use default settings if file does not exist or is corrupted (backed up above already in the latter case)
     }
 
     static final void save() {
+        DarkUtilsConfig.save(DarkUtilsConfig.FILE.toPath());
+    }
+
+    private static final void save(@NotNull final Path path) {
         ConfigSaveStartEvent.INSTANCE.trigger();
 
         try {
-            Files.writeString(DarkUtilsConfig.FILE.toPath(), DarkUtilsConfig.GSON.toJson(DarkUtilsConfig.INSTANCE), StandardCharsets.UTF_8);
+            Files.writeString(path, DarkUtilsConfig.GSON.toJson(DarkUtilsConfig.INSTANCE), StandardCharsets.UTF_8);
         } catch (final IOException e) {
             DarkUtils.error(DarkUtilsConfig.class, "Unable to save config", e);
         } finally {
