d6af79e865b79b5df9008d5a97a734d6020e9f32
Mustafa ÖNCEL <mustafaoncel92@gmail.com>
2026-03-04 06:53:29 +0300

    Try to speedup gradle build

--- src/test/java/gg/darkutils/ChatUtilsTest.java
+++ src/test/java/gg/darkutils/ChatUtilsTest.java
@@ -7,33 +7,33 @@ import org.junit.jupiter.api.Assertions;
 
 final class ChatUtilsTest {
     @Test
-    void removeControlCodes_emptyString_returnsEmpty() {
+    final void removeControlCodes_emptyString_returnsEmpty() {
         Assertions.assertEquals("", ChatUtils.removeControlCodes(""));
     }
 
     @Test
-    void removeControlCodes_noControlCodes_returnsSameInstance() {
+    final void removeControlCodes_noControlCodes_returnsSameInstance() {
         final var text = "Hello world";
 
         Assertions.assertSame(text, ChatUtils.removeControlCodes(text));
     }
 
     @Test
-    void removeControlCodes_singleCode_removed() {
+    final void removeControlCodes_singleCode_removed() {
         final var input = "Hello §aWorld";
 
         Assertions.assertEquals("Hello World", ChatUtils.removeControlCodes(input));
     }
 
     @Test
-    void removeControlCodes_multipleCodes_removed() {
+    final void removeControlCodes_multipleCodes_removed() {
         final var input = "§aHello §bWorld§r!";
 
         Assertions.assertEquals("Hello World!", ChatUtils.removeControlCodes(input));
     }
 
     @Test
-    void removeControlCodes_trailingControlCode_doesNotThrow() {
+    final void removeControlCodes_trailingControlCode_doesNotThrow() {
         final var input = "Hello§";
 
         Assertions.assertEquals("Hello", ChatUtils.removeControlCodes(input));
