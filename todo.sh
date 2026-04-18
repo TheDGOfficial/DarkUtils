756e662c820ac28c72b43ee59d30c0e44c7ad314
Mustafa ÖNCEL <mustafaoncel92@gmail.com>
2026-04-18 11:04:17 +0300

    Fix todo.sh error

--- todo.sh
+++ todo.sh
@@ -6,7 +6,9 @@ trap 'CODE=$?; echo "$0: Error on line $LINENO: $BASH_COMMAND"; exit $CODE' ERR
 IFS=$'\n\t'
 
 # Grep all todo or fixme containing files
-./search.sh "TODO|FIXME"
-EXIT_CODE=$?
-exit $EXIT_CODE
+if ./search.sh "TODO|FIXME"; then
+    exit 0
+else
+    exit 1
+fi
 
