a1b27ece30cf53dfb7f2ed0df57f19730f4fc4fc
Mustafa ÖNCEL <mustafaoncel92@gmail.com>
2026-04-19 22:14:42 +0300

    Fix more renamed Mixin methods

--- src/main/java/gg/darkutils/mixin/visuals/GuiMixin.java
+++ src/main/java/gg/darkutils/mixin/visuals/GuiMixin.java
@@ -24,14 +24,14 @@ final class GuiMixin {
         throw new UnsupportedOperationException("mixin class");
     }
 
-    @Inject(method = "renderArmor", at = @At("HEAD"), cancellable = true)
+    @Inject(method = "extractArmor", at = @At("HEAD"), cancellable = true)
     private static final void darkutils$skipRenderingArmorProtectionPointsHudIfEnabled(@NotNull final GuiGraphicsExtractor context, @NotNull final Player player, final int i, final int j, final int k, final int x, @NotNull final CallbackInfo ci) {
         if (DarkUtilsConfig.INSTANCE.hideArmorAndFood) {
             ci.cancel();
         }
     }
 
-    @Redirect(method = "renderTabList", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/KeyMapping;isDown()Z"))
+    @Redirect(method = "extractTabList", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/KeyMapping;isDown()Z"))
     private final boolean darkutils$showTabListIfEnabled(@NotNull final KeyMapping keyBinding) {
         if (Minecraft.getInstance().options.keyPlayerList != keyBinding) {
             throw new IllegalStateException("@fileName@ needs updating (" + DarkUtils.class.getSimpleName() + ')');
@@ -40,7 +40,7 @@ final class GuiMixin {
         return keyBinding.isDown() || DarkUtilsConfig.INSTANCE.persistentTabListWhileFarming && ActivityState.isActivelyFarming() && LocationUtils.isInGarden();
     }
 
-    @Inject(at = @At("HEAD"), method = "renderEffects", cancellable = true)
+    @Inject(at = @At("HEAD"), method = "extractEffects", cancellable = true)
     private final void darkutils$skipRenderingEffectsHudIfEnabled(@NotNull final CallbackInfo ci) {
         if (DarkUtilsConfig.INSTANCE.hideEffectsHud) {
             // cancel rendering effects in top right corner of the screen all the time
@@ -48,14 +48,14 @@ final class GuiMixin {
         }
     }
 
-    @Inject(method = "renderFood", at = @At("HEAD"), cancellable = true)
+    @Inject(method = "extractFood", at = @At("HEAD"), cancellable = true)
     private final void darkutils$skipRenderFoodIfEnabled(@NotNull final GuiGraphicsExtractor context, @NotNull final Player player, final int top, final int right, @NotNull final CallbackInfo ci) {
         if (DarkUtilsConfig.INSTANCE.hideArmorAndFood) {
             ci.cancel();
         }
     }
 
-    @Inject(method = "renderVehicleHealth", at = @At("HEAD"), cancellable = true)
+    @Inject(method = "extractVehicleHealth", at = @At("HEAD"), cancellable = true)
     private final void darkutils$skipRenderMountHealthIfEnabled(@NotNull final GuiGraphicsExtractor context, @NotNull final CallbackInfo ci) {
         if (DarkUtilsConfig.INSTANCE.hideMountHealth) {
             ci.cancel();
